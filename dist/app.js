cb51717cfdd25de1683c8d9be3795f87c47d4234
sjulian25 <juliansalgado1392@gmail.com>
2024-09-04 18:41:12 -0500

    Se añade glider en la sección de proyectos y hacen pequeños cambios en secciones anteriores

--- dist/app.js
+++ dist/app.js
@@ -85,3 +85,40 @@ jobLinks.forEach((link) => {
         firstLink.classList.remove('dark:bg-slate-700', 'bg-white', 'border-sky-500');
     });
 });
+
+// Carousel
+window.addEventListener('load', function(){
+    new Glider(this.document.querySelector('.carousel__lista'), {
+        // Mobile-first defaults
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        scrollLock: true,
+        dots: '.carousel__indicadores',
+        arrows: {
+            prev: '.carousel__anterior',
+            next: '.carousel__siguiente'
+        },
+        responsive: [
+            {
+            // screens greater than >= 775px
+            breakpoint: 768,
+            settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+            }
+            },{
+            // screens greater than >= 1024px
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+            }
+            }
+        ]
+    });
+});
